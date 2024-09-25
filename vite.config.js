c2efa21ee6115e50a6ab12e12313835e43c43425
Art__Attack <164230299+ashifhard@users.noreply.github.com>
2024-09-25 10:34:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['bootstrap'], // Exclude 'bootstrap' from the bundle
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import 'bootstrap/dist/css/bootstrap.min.css';`,
+      },
     },
   },
 });
